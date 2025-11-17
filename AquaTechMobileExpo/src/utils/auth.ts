09612417f61c926928069ca3cbbab964fc35570d
godwin <godwintamayo44@gmail.com>
2025-11-17 15:17:42 +0800

    google account chooser

--- AquaTechMobileExpo/src/utils/auth.ts
+++ AquaTechMobileExpo/src/utils/auth.ts
@@ -1,9 +1,12 @@
 import auth from '@react-native-firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { resetToAuth } from '../navigation/navigationRef';
 
 export async function logout() {
   try {
+    await GoogleSignin.revokeAccess();
+    await GoogleSignin.signOut();
     await auth().signOut();
   } catch {}
   try {
